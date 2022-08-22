82f0bf5395f6a2394b6b800cd555718dfbd3d8d5
fefragoso <felipeofragoso@gmail.com>
2022-08-22 15:25:19 -0300

    Melhorias de estilo e adição dos botões

--- App.js
+++ App.js
@@ -19,12 +19,23 @@ export default function App() {
 
       <Text style={s.frase}>" Teste de frase! "</Text>
 
-      <TouchableOpacity
-        style={s.btnQuebra}
-        onPress={ ()=> alert('Foi!') }
-      >
-        <Text>Quebrar Biscoito</Text>
-      </TouchableOpacity>
+      <View>
+
+        <TouchableOpacity
+          style={[s.btnQuebra, { marginBottom: 0 }]}
+          onPress={ ()=> alert('Foi!') }
+        >
+          <Text style={s.btnTexto}>Quebrar Biscoito</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[s.btnQuebra, { borderColor: '#121212' }]}
+          onPress={ ()=> alert('Foi!') }
+        >
+          <Text style={[s.btnTexto, { color: '#121212' }]}>Reiniciar Biscoito</Text>
+        </TouchableOpacity>
+
+      </View>
 
     </View>
 
@@ -47,6 +58,23 @@ const s = EStyleSheet.create({
     fontSize: '1rem',
     fontStyle: 'italic',
     textAlign: 'center',
+
+    color: '#dd7b22'
+  },
+  btnQuebra: {
+    margin: '1.5rem',
+    padding: '0.75rem',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderWidth: 2,
+    borderRadius: 25,
+    borderColor: '#dd7b22'
+  },
+  btnTexto: {
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
 
     color: '#dd7b22'
   }
